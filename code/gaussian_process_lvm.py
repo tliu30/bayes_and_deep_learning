d1e9eedabe87b97e43214f5b25574a66eac7d932
Anthony Liu <anthony.liu@kensho.com>
2017-12-10 10:41:50 -0500

    first pass on inactive step

--- code/gaussian_process_lvm.py
+++ code/gaussian_process_lvm.py
@@ -23,7 +23,7 @@ import torch
 
 # Kernel functions
 from code.linear_regression_lvm import make_torch_variable
-from code.mvn import torch_determinant, torch_gp_mvn_log_density
+from code.mvn import torch_determinant, torch_gp_mvn_log_density, torch_mvn_density
 
 
 def rbf_kernel_forward(x1, x2, log_lengthscale, eps=1e-5):
@@ -62,15 +62,27 @@ def rbf_kernel_forward(x1, x2, log_lengthscale, eps=1e-5):
 MLE_PARAMS = namedtuple('MLE_PARAMS', ['z', 'alpha', 'sigma', 'log_l'])
 
 
-def _mle_likelihood(x, z, alpha, sigma, log_l, eps=1e-5):
+def _make_cov(x1, x2, alpha, sigma, log_l):
+    n1, _ = x1.size()
+    n2, _ = x2.size()
+
+    inner_product = rbf_kernel_forward(x1, x2, log_l)
+    identity = make_torch_variable(np.identity(n1)[:n1, :n2], False)
+
+    a1 = torch.mul(alpha ** 2, inner_product)
+    a2 = torch.mul(sigma ** 2, identity)
+    cov = torch.add(a1, a2)
+
+    return cov
+
+
+def _mle_log_likelihood(x, z, alpha, sigma, log_l):
     '''Note we use just one lengthscale across every dimension'''
     n, m1 = x.size()
     _, m2 = z.size()
 
     # Compute covariance matrix of each dimension (shape n x n)
-    inner_product = rbf_kernel_forward(z, z, log_l, eps=eps)
-    identity = make_torch_variable(np.identity(n), True)
-    cov = torch.add(torch.mul(alpha ** 2, inner_product), torch.mul(sigma ** 2, identity))
+    cov = _make_cov(z, z, alpha, sigma, log_l)
 
     # Compute log lik
     approx_marginal_log_likelihood = torch_gp_mvn_log_density(x, cov)
@@ -78,10 +90,10 @@ def _mle_likelihood(x, z, alpha, sigma, log_l, eps=1e-5):
     return approx_marginal_log_likelihood
 
 
-def mle_batch_likelihood(x, mle_params, batch_ix):
+def mle_batch_log_likelihood(x, mle_params, batch_ix):
     batch_x = x[batch_ix, :]
     batch_z = mle_params.z[batch_ix, :]
-    return _mle_likelihood(batch_x, batch_z, mle_params.alpha, mle_params.sigma, mle_params.log_l)
+    return _mle_log_likelihood(batch_x, batch_z, mle_params.alpha, mle_params.sigma, mle_params.log_l)
 
 
 def mle_forward_step_w_optim(x, mle_params, b, optimizer):
@@ -91,7 +103,7 @@ def mle_forward_step_w_optim(x, mle_params, b, optimizer):
     batch_ix = np.random.choice(range(n), b, replace=True)
 
     # Estimate marginal likelihood
-    neg_log_lik = -1 * mle_batch_likelihood(x, mle_params, batch_ix)
+    neg_log_lik = -1 * mle_batch_log_likelihood(x, mle_params, batch_ix)
 
     # Do a backward step
     neg_log_lik.backward()
@@ -114,11 +126,41 @@ def mle_forward_step_w_optim(x, mle_params, b, optimizer):
 # Implement Lawrence GPLVM active / inactive algorithm
 
 
-def _active_step(x, mle_params, active_ix, optimizer_kernel):
+def _inactive_step(x, mle_params, active_ix, inactive_ix):
+    _, m1 = x.size()
+
     active_x = x[active_ix, :]
     active_z = mle_params.z[active_ix, :]
+    active_n, _ = active_x.size()
 
+    active_cov = _make_cov(active_z, active_z, mle_params.alpha, mle_params.sigma, mle_params.log_l)
+    active_cov_inv = torch.inverse(active_cov)
 
+    inactive_log_lik = make_torch_variable([0], requires_grad=False)
+    for i in inactive_ix:
+        inactive_x = x[[i], :]
+        inactive_z = mle_params.z[[i], :]
+
+        active_inactive_cov = _make_cov(active_z, inactive_z, mle_params.alpha, mle_params.sigma,
+                                        mle_params.log_l)
+        inactive_var = _make_cov(inactive_z, inactive_z, mle_params.alpha, mle_params.sigma,
+                                 mle_params.log_l)
+
+        # TODO: Do not have dimensions right here...
+        # (M1 x 1) x (active_n x active_n) x (active_n x 1) --> (M1 x 1)
+        inactive_mu = torch.mm(inactive_x.t(), torch.mm(active_cov_inv, active_inactive_cov))
+
+        # (1 x active_n) x (active_n x active_n) x (active_n x 1) --> (1 x 1)
+        a1 = torch.mm(active_inactive_cov.t(), torch.mm(active_cov_inv, active_inactive_cov))
+        identity = make_torch_variable(np.identity(m1), False)
+        inactive_sigma = torch.mul(torch.add(inactive_var, -1 * a1), identity)
+
+        # Compute log likelihood of current instance
+        instance_log_lik = torch_mvn_density(inactive_x, inactive_mu, inactive_sigma, log=True)
+
+        inactive_log_lik = torch.add(inactive_log_lik, instance_log_lik)
+
+    return inactive_log_lik
 
 
 def mle_active_inactive_step_w_optim(x, mle_params, b, optimizer_kernel, optimizer_latent):
@@ -126,8 +168,22 @@ def mle_active_inactive_step_w_optim(x, mle_params, b, optimizer_kernel, optimiz
     # Create batch (they use informative vector machine [IVM] but i'm lazy)
     n, _ = x.shape
     active_ix = np.random.choice(range(n), b, replace=False)  # w/o replacement!
+    inactive_ix = [x for x in range(n) if x not in active_ix]
 
     # Optimize kernel parameters given the active set
+    # TODO: refactor mle_forward_step_w_optim to do this?
+    neg_active_log_lik = -1 * mle_batch_log_likelihood(x, mle_params, active_ix)
 
+    neg_active_log_lik.backward()
+    optimizer_kernel.step()
 
+    mle_params.log_l.data[0] = max(-10000, mle_params.log_l.data[0])
+    mle_params.log_l.data[0] = min(10000, mle_params.log_l.data[0])
+    mle_params.sigma.data[0] = max(1e-10, mle_params.sigma.data[0])
+    mle_params.alpha.data[0] = max(1e-10, mle_params.alpha.data[0])
 
+    # Optimize latent parameters of inactive set
+    neg_inactive_log_lik = -1 * _inactive_step(x, mle_params, active_ix, inactive_ix)
+
+    neg_inactive_log_lik.backward()
+    optimizer_latent.step()
