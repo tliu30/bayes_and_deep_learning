fa40d6483ebb1039f78685dd7b6a2b4b6bee5819
Anthony Liu <anthony.liu@kensho.com>
2017-12-10 09:44:06 -0500

    updated todos & naive gplvm

---
+++ code/gaussian_process_lvm.py
@@ -0,0 +1,115 @@
+#
+# Latent variable models of form
+#    z_i ~ MVN(0, I)
+#      f ~ GP(0, K(z_1, z_2))
+#    x_i ~ MVN(f(z_i), sigma^2 * I)
+# where z_i is R^M2 and x_i is R^M1
+#
+# I think there's an assumption here of x being centered & isotropic
+#
+# We assume all vectors are row-vectors; hence in the above, we have shapes:
+#    z - (N, M2)
+#    x - (N, M1)
+#
+# We implement two methods:
+#    (1) fitting with maximum likelihood w.r.t. z (e.g., marginalize out f)
+#    (2) variational inference with q(z_i | x_i) = N(g(x_i), tau^2 ** I)
+#        where q ~ GP(0, K'(x_1, x_2))
+#
+from collections import namedtuple
+
+import numpy as np
+import torch
+
+# Kernel functions
+from code.linear_regression_lvm import make_torch_variable
+from code.mvn import torch_determinant
+
+
+def rbf_kernel_forward(x1, x2, log_lengthscale, eps=1e-5):
+    '''Compute the distance matrix under rbf kernel; taken from github repo jrg365/gpytorch
+
+    e.g., K(x, x') = exp( - ||x - x'||^2 / (2 * sigma**2) )
+
+    Args:
+        x1: (torch variable) shape n x d
+        x2: (torch variable) shape m x d
+        log_lengthscale: (torch variable) log of the hyperparameter l
+        eps: (float) a safety term to make the lengthscale not zero (since is in denominator)
+
+    Returns:
+        distance matrix of shape (n x m)
+    '''
+    n, d = x1.size()
+    m, _ = x2.size()
+
+    res = 2 * x1.matmul(x2.transpose(0, 1))
+
+    x1_squared = torch.bmm(x1.view(n, 1, d), x1.view(n, d, 1))
+    x1_squared = x1_squared.view(n, 1).expand(n, m)
+    x2_squared = torch.bmm(x2.view(m, 1, d), x2.view(m, d, 1))
+    x2_squared = x2_squared.view(1, m).expand(n, m)
+    res.sub_(x1_squared).sub_(x2_squared)  # res = -(x - z)^2
+
+    res = res / (log_lengthscale.exp() + eps)  # res = -(x - z)^2 / lengthscale
+    res.exp_()
+
+    return res
+
+
+# Maximum likelihood w.r.t. z
+
+MLE_PARAMS = namedtuple('MLE_PARAMS', ['z', 'alpha', 'sigma', 'log_l'])
+
+
+def _mle_likelihood(x, z, alpha, sigma, log_l, eps=1e-5):
+    '''Note we use just one lengthscale across every dimension'''
+    n, m1 = x.size()
+    _, m2 = z.size()
+
+    # Compute covariance matrix of each dimension (shape n x n)
+    inner_product = rbf_kernel_forward(z, z, log_l, eps=eps)
+    identity = make_torch_variable(np.identity(n), True)
+    cov = torch.add(torch.mul(alpha ** 2, inner_product), torch.mul(sigma ** 2, identity))
+
+    # Compute log lik
+    cov_det = torch_determinant(cov)
+    cov_inv = torch.inverse(cov)
+
+    a_1 = np.log(cov_det)
+    a_2 = torch.mm(cov_inv, torch.mm(x, x.t())).diag().sum()
+    approx_marginal_log_likelihood = a_1 + a_2
+
+    return approx_marginal_log_likelihood
+
+
+def mle_batch_likelihood(x, mle_params, batch_ix):
+    batch_x = x[batch_ix, :]
+    batch_z = mle_params.z[batch_ix, :]
+    return _mle_likelihood(batch_x, batch_z, mle_params.alpha, mle_params.sigma, mle_params.log_l)
+
+
+def mle_forward_step_w_optim(x, mle_params, b, optimizer):
+    # Create batch
+    n, _ = x.shape
+    batch_ix = np.random.choice(range(n), b, replace=True)
+
+    # Estimate marginal likelihood
+    neg_log_lik = -1 * mle_batch_likelihood(x, mle_params, batch_ix)
+
+    # Do a backward step
+    neg_log_lik.backward()
+
+    # Do an optimization step
+    optimizer.step()
+
+    # Enforce bounds on lengthscale and sigmas
+    mle_params.log_l.data[0] = max(-10000, mle_params.log_l.data[0])
+    mle_params.log_l.data[0] = min(10000, mle_params.log_l.data[0])
+    mle_params.sigma.data[0] = max(1e-10, mle_params.sigma.data[0])
+    mle_params.alpha.data[0] = max(1e-10, mle_params.alpha.data[0])
+
+    # Clear gradients
+    optimizer.zero_grad()
+
+    return mle_params, neg_log_lik
--- todo.md
+++ todo.md
@@ -1,25 +1,35 @@
 Some experiments to run to make sure I know what's going on
 
 x. Make sure I know how torch autodifferentiation works
+
     x. First, fit a normal distribution using maximum likelihood
     x. Second, fit a multivariate normal distribution using maximum likelihood
 
 x. Understand why the reparametrization trick helps
+
     x. Compute gradient estimates of parameter in distribution using
+    
         x. the variational approximation
         x. the reparametrization
 
 x. Implement PPCA with MLE
+
     x unclear if correct...
-    * should be able to implement with EM (read Tipping / Bishop)
+    x should be able to implement with EM (read Tipping / Bishop)
     * also GPLVM - integrating out the coefficient matrix (Lawrence)
+        * e.g., most importantly, implement PPCA with beta marginalized out
     x use adam rather than hand-implemented gradient descent
+    
+x. Implement PPCA with EM
 
 x. Implement PPCA with VB
+
     x unclear if correct...
     x use adam rather than hand-implemented gradient descent
 
 3. Implement GPLVM with MLE
+
+    * take a look at https://github.com/jrg365/gpytorch to see a good implementation
 
 4. Implement GPLVM with VB
 
