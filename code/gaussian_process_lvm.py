974e97cd5a65aa8989d765827d70c0fdda90d57d
Anthony Liu <anthony.liu@kensho.com>
2017-12-11 08:19:09 -0500

    fix lawrence algo

--- code/gaussian_process_lvm.py
+++ code/gaussian_process_lvm.py
@@ -126,41 +126,60 @@ def mle_forward_step_w_optim(x, mle_params, b, optimizer):
 # Implement Lawrence GPLVM active / inactive algorithm
 
 
-def _inactive_step(x, mle_params, active_ix, inactive_ix):
-    _, m1 = x.size()
+def _inactive_point_likelihood(active_x, active_z, inactive_x, inactive_z, alpha, sigma, log_l):
+    '''Compute the likelihood of the inactive set conditional on the active set'''
+    # Extract dimensions
+    active_n, m1 = active_x.size()
+    inactive_n, _ = inactive_x.size()
 
-    active_x = x[active_ix, :]
-    active_z = mle_params.z[active_ix, :]
-    active_n, _ = active_x.size()
-
-    active_cov = _make_cov(active_z, active_z, mle_params.alpha, mle_params.sigma, mle_params.log_l)
+    # Construct covariance of active set & functions thereof that get reused
+    active_cov = _make_cov(active_z, active_z, alpha, sigma, log_l)
     active_cov_inv = torch.inverse(active_cov)
+    mean_component = torch.mm(active_x.t(), active_cov_inv)  # Dim (M1 x active_n)
 
+    # Compute likelihood of inactive set conditional on active set
+    # (we iterate since (1) all computations are independent and (2) for clarity)
     inactive_log_lik = make_torch_variable([0], requires_grad=False)
-    for i in inactive_ix:
-        inactive_x = x[[i], :]
-        inactive_z = mle_params.z[[i], :]
+    for i in range(inactive_n):
 
-        active_inactive_cov = _make_cov(active_z, inactive_z, mle_params.alpha, mle_params.sigma,
-                                        mle_params.log_l)
-        inactive_var = _make_cov(inactive_z, inactive_z, mle_params.alpha, mle_params.sigma,
-                                 mle_params.log_l)
+        # Extract current pair
+        cur_x = inactive_x[[i], :]
+        cur_z = inactive_z[[i], :]
 
-        # TODO: Do not have dimensions right here...
-        # (M1 x 1) x (active_n x active_n) x (active_n x 1) --> (M1 x 1)
-        inactive_mu = torch.mm(inactive_x.t(), torch.mm(active_cov_inv, active_inactive_cov))
+        # Compute covariance of inactive point with active set, as well as with itself
+        active_inactive_cov = _make_cov(active_z, cur_z, alpha, sigma, log_l)  # Dim (active_n x 1)
+        inactive_var = _make_cov(cur_z, cur_z, alpha, sigma, log_l)  # Dim (1 x 1)
 
-        # (1 x active_n) x (active_n x active_n) x (active_n x 1) --> (1 x 1)
+        # Compute E[cur_x] = t(active_x) * inv(active(cov)) * active_inactive_cov
+        # Dim (m1 x active_n) * (active_n x active_n) * (active_n x 1) --> (m1 x 1)
+        inactive_mu = torch.mm(mean_component, active_inactive_cov)
+
+        # Compute Cov(cur_x) = (sigma ** 2) * I
+        # Dim of sigma (1 x 1) - (1 x active_n) * (active_n x active_n) * (active_n x 1) --> (1 x 1)
         a1 = torch.mm(active_inactive_cov.t(), torch.mm(active_cov_inv, active_inactive_cov))
         identity = make_torch_variable(np.identity(m1), False)
         inactive_sigma = torch.mul(torch.add(inactive_var, -1 * a1), identity)
 
         # Compute log likelihood of current instance
-        instance_log_lik = torch_mvn_density(inactive_x, inactive_mu, inactive_sigma, log=True)
+        instance_log_lik = torch_mvn_density(cur_x, inactive_mu, inactive_sigma, log=True)
 
+        # Add to overall log likelihood
         inactive_log_lik = torch.add(inactive_log_lik, instance_log_lik)
 
     return inactive_log_lik
+
+
+def inactive_point_likelihood(x, mle_params, active_ix, inactive_ix):
+    active_x = x[active_ix, :]
+    active_z = mle_params.z[active_ix, :]
+
+    inactive_x = x[inactive_ix, :]
+    inactive_z = mle_params.z[inactive_ix, :]
+
+    return _inactive_point_likelihood(
+        active_x, active_z, inactive_x, inactive_z,
+        mle_params.alpha, mle_params.sigma, mle_params.log_l
+    )
 
 
 def mle_active_inactive_step_w_optim(x, mle_params, b, optimizer_kernel, optimizer_latent):
@@ -183,7 +202,18 @@ def mle_active_inactive_step_w_optim(x, mle_params, b, optimizer_kernel, optimiz
     mle_params.alpha.data[0] = max(1e-10, mle_params.alpha.data[0])
 
     # Optimize latent parameters of inactive set
-    neg_inactive_log_lik = -1 * _inactive_step(x, mle_params, active_ix, inactive_ix)
+    # TODO: do i need to do some splitting here so it doesn't try to optimize active latents?
+    neg_inactive_log_lik = -1 * inactive_point_likelihood(x, mle_params, active_ix, inactive_ix)
 
     neg_inactive_log_lik.backward()
     optimizer_latent.step()
+
+    return mle_params, neg_active_log_lik, neg_inactive_log_lik
+
+
+# Variational bayes version
+
+VB_PARAMS = namedtuple(
+    'VB_PARAMS',
+    ['z', 'alpha', 'sigma', 'log_l', 'alpha_q', 'sigma_q', 'log_l_q']
+)
