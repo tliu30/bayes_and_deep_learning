ba3642c9bf708b8e9c99f96213d3b07afefcc790
Anthony Liu <anthony.liu@kensho.com>
2017-12-16 10:10:47 -0500

    draft a vb method

--- code/gaussian_process_lvm.py
+++ code/gaussian_process_lvm.py
@@ -126,6 +126,44 @@ def mle_forward_step_w_optim(x, mle_params, b, optimizer):
 # Implement Lawrence GPLVM active / inactive algorithm
 
 
+def _gp_conditional_mean_cov(cur_z, active_x, active_z, alpha, sigma, log_l,
+                             active_cov=None, active_cov_inv=None, mean_component=None):
+    '''Compute E[x] and Var(x) where x ~ N(f(cur_z), sigma) and f | active ~ GP(0, alpha, l)
+
+    In other words, compute the mean and variance of x given it's latent point z conditioned
+    on the active set.
+    '''
+    # Extract dimensions
+    active_n, m1 = active_x.size()
+    cur_n, _ = cur_z.size()
+
+    if cur_n > 1:
+        raise ValueError('At the moment we just do one inactive point at a time...')
+
+    # Construct covariance of active set & functions thereof that get reused
+    precomputed = not ((active_cov is None) | (active_cov_inv is None) | (mean_component is None))
+    if not precomputed:
+        active_cov = _make_cov(active_z, active_z, alpha, sigma, log_l)
+        active_cov_inv = torch.inverse(active_cov)
+        mean_component = torch.mm(active_x.t(), active_cov_inv)  # Dim (M1 x active_n)
+
+    # Compute covariance of inactive point with active set, as well as with itself
+    active_inactive_cov = _make_cov(active_z, cur_z, alpha, sigma, log_l)  # Dim (active_n x 1)
+    inactive_var = _make_cov(cur_z, cur_z, alpha, sigma, log_l)  # Dim (1 x 1)
+
+    # Compute E[cur_x] = t(active_x) * inv(active(cov)) * active_inactive_cov
+    # Dim (m1 x active_n) * (active_n x active_n) * (active_n x 1) --> (m1 x 1)
+    inactive_mu = torch.mm(mean_component, active_inactive_cov)
+
+    # Compute Cov(cur_x) = (sigma ** 2) * I
+    # Dim of sigma (1 x 1) - (1 x active_n) * (active_n x active_n) * (active_n x 1) --> (1 x 1)
+    a1 = torch.mm(active_inactive_cov.t(), torch.mm(active_cov_inv, active_inactive_cov))
+    identity = make_torch_variable(np.identity(m1), False)
+    inactive_sigma = torch.mul(torch.add(inactive_var, -1 * a1), identity)
+
+    return inactive_mu, inactive_sigma
+
+
 def _inactive_point_likelihood(active_x, active_z, inactive_x, inactive_z, alpha, sigma, log_l):
     '''Compute the likelihood of the inactive set conditional on the active set'''
     # Extract dimensions
@@ -146,21 +184,11 @@ def _inactive_point_likelihood(active_x, active_z, inactive_x, inactive_z, alpha
         cur_x = inactive_x[[i], :]
         cur_z = inactive_z[[i], :]
 
-        # Compute covariance of inactive point with active set, as well as with itself
-        active_inactive_cov = _make_cov(active_z, cur_z, alpha, sigma, log_l)  # Dim (active_n x 1)
-        inactive_var = _make_cov(cur_z, cur_z, alpha, sigma, log_l)  # Dim (1 x 1)
-
-        # Compute E[cur_x] = t(active_x) * inv(active(cov)) * active_inactive_cov
-        # Dim (m1 x active_n) * (active_n x active_n) * (active_n x 1) --> (m1 x 1)
-        inactive_mu = torch.mm(mean_component, active_inactive_cov)
-
-        # Compute Cov(cur_x) = (sigma ** 2) * I
-        # Dim of sigma (1 x 1) - (1 x active_n) * (active_n x active_n) * (active_n x 1) --> (1 x 1)
-        a1 = torch.mm(active_inactive_cov.t(), torch.mm(active_cov_inv, active_inactive_cov))
-        identity = make_torch_variable(np.identity(m1), False)
-        inactive_sigma = torch.mul(torch.add(inactive_var, -1 * a1), identity)
-
         # Compute log likelihood of current instance
+        inactive_mu, inactive_sigma = _gp_conditional_mean_cov(
+            cur_z, active_x, active_z, alpha, sigma, log_l,
+            active_cov=active_cov, active_cov_inv=active_cov_inv, mean_component=mean_component
+        )
         instance_log_lik = torch_mvn_density(cur_x, inactive_mu, inactive_sigma, log=True)
 
         # Add to overall log likelihood
@@ -211,9 +239,145 @@ def mle_active_inactive_step_w_optim(x, mle_params, b, optimizer_kernel, optimiz
     return mle_params, neg_active_log_lik, neg_inactive_log_lik
 
 
-# Variational bayes version
+# Variational bayes version - where q(z) ~ N(g(x), sigma), g ~ GP
 
 VB_PARAMS = namedtuple(
     'VB_PARAMS',
-    ['z', 'alpha', 'sigma', 'log_l', 'alpha_q', 'sigma_q', 'log_l_q']
+    ['alpha', 'sigma', 'log_l', 'alpha_q', 'sigma_q', 'log_l_q']
 )
+
+
+def _vb_lower_bound(x, noise, alpha, sigma, log_l, alpha_q, sigma_q, log_l_q):
+
+    B, M1 = x.size()
+    _, M2 = noise.size()
+
+    # Posterior under variational approximation
+    cov_z = _make_cov(x, x, alpha_q, sigma_q, log_l_q)  # for f_q: X --> Z
+    log_posterior = torch_gp_mvn_log_density(noise, cov_z)
+
+    # Likelihood under model
+    cov_x = _make_cov(noise, noise, alpha, sigma, log_l)  # for f: Z --> X
+    log_likelihood = torch_gp_mvn_log_density(x, cov_x)
+
+    # Prior under model
+    mu_prior = make_torch_variable(np.zeros(M2), False)
+    sigma_prior = make_torch_variable(np.identity(M2), False)
+    log_prior = torch_mvn_density(noise, mu_prior, sigma_prior, log=True)
+
+    # Compute lower bound
+    lower_bound = log_posterior - log_likelihood - log_prior
+
+    return lower_bound.sum()
+
+
+def vb_lower_bound(x, noise, vb_params):
+    return _vb_lower_bound(
+        x, noise,
+        vb_params.alpha, vb_params.sigma, vb_params.log_l,
+        vb_params.alpha_q, vb_params.sigma_q, vb_params.log_l_q
+    )
+
+
+def _reparametrize_noise(inactive_x, inactive_noise, active_x, active_z, alpha_q, sigma_q, log_l_q):  # Don't i need to choose an active set?
+    '''Using an active set to give some definition to GP draw, reparam N(0, 1) noise given batch'''
+    # Extract dimensions
+    active_n, m1 = active_x.size()
+    inactive_n, _ = inactive_x.size()
+
+    # Construct covariance of active set & functions thereof that get reused
+    active_cov = _make_cov(active_x, active_x, alpha_q, sigma_q, log_l_q)
+    active_cov_inv = torch.inverse(active_cov)
+    mean_component = torch.mm(active_z.t(), active_cov_inv)  # Dim (M1 x active_n)
+
+    # Compute likelihood of inactive set conditional on active set
+    # (we iterate since (1) all computations are independent and (2) for clarity)
+    reparametrized_points = []
+    for i in range(inactive_n):
+
+        # Extract current pair
+        cur_x = inactive_x[[i], :]
+        cur_noise = inactive_noise[[i], :]
+
+        inactive_mu, inactive_sigma = _gp_conditional_mean_cov(
+            cur_x, active_z, active_x, alpha_q, sigma_q, log_l_q,
+            active_cov=active_cov, active_cov_inv=active_cov_inv, mean_component=mean_component
+        )
+
+        reparametrized_points.append(torch.add(inactive_mu, torch.mul(inactive_sigma, cur_noise)))
+
+    return torch.cat(reparametrized_points, dim=1)  # The dimension might be wrong here
+
+
+def reparametrize_noise(inactive_x, inactive_noise, active_x, active_z, vb_params):
+    return _reparametrize_noise(
+        inactive_x, inactive_noise, active_x, active_z,
+        vb_params.alpha_q, vb_params.sigma_q, vb_params.log_l_q
+    )
+
+
+def resample_latent_space(batch_x, active_x, active_z, vb_params):
+    '''e.g., resample latent points z corresponding to batch variables given an active set'''
+    n_batch, _ = batch_x.size()
+    _, m2 = active_z.size()
+
+    # Do this to get the mean...alternatively, could sample to introduce noise
+    noise = make_torch_variable(np.zeros((n_batch, m2)), requires_grad=False)
+
+    return reparametrize_noise(batch_x, noise, active_x, active_z, vb_params)
+
+
+def vb_forward_step(x, z, vb_params, n_active, n_batch, optimizer):
+    '''The iterative step for a gaussian process latent variable model with variational inference
+
+    Our goal here was to imitate the setup used by D.P. Kingma for AEVB, e.g.,
+        1. Define the likelihood as x ~ N(f(z), sigma) where f: Z -> X ~ P(.)
+        2. Define the variational approximation as part of the same family of distributions as the
+           likelihood, e.g., z ~ q(z | x) = N(f_q(x), sigma_q) where g: X -> Z ~ P_q(.)
+
+    However, it's a bit odd for gaussian processes because unlike AEVB - where P & P_q are
+    parametrized by the weights of the neural network, and therefore which establish a well-defined
+    mean f(z) & f_q(x) - the gaussian process framework often has a mean of zero unless we condition
+    it on an active set...
+
+    For that reason, we try to hack an approach together where we establish an active set before
+    computing the variational lower bound, as it creates a more meaningful function...
+    '''
+    # Select an active set
+    n, m1 = x.size()
+    _, m2 = z.size()
+
+    active_ix = np.random.choice(x, size=n_active, replace=False)
+
+    active_x = x[active_ix, :]
+    active_z = z[active_ix, :]
+
+    # Select a batch from inactive set
+    inactive_ix = np.array([x for x in range(n) if x not in active_ix])
+    batch_ix = np.random.choice(inactive_ix, size=n_batch, replace=True)
+
+    batch_x = x[batch_ix, :]
+
+    # Sample noise for batch (sample as MVN(0, I))
+    noise = np.random.randn(n, m2)
+
+    # Reparametrize noise given an active set
+    batch_z = reparametrize_noise(batch_x, noise, active_x, active_z, vb_params)
+
+    # Compute variational lower bound & update parameters
+    lower_bound = vb_lower_bound(batch_x, batch_z, vb_params)
+
+    lower_bound.backward()
+    optimizer.step()
+
+    # Constrain sigma & alpha
+    vb_params.sigma.data[0] = max(1e-10, vb_params.sigma.data[0])
+    vb_params.alpha.data[0] = max(1e-10, vb_params.alpha.data[0])
+    vb_params.sigma_q.data[0] = max(1e-10, vb_params.sigma_q.data[0])
+    vb_params.alpha_q.data[0] = max(1e-10, vb_params.alpha_q.data[0])
+
+    # Clear gradients
+    optimizer.zero_grad()
+
+    # Update latent variables for inactive set given new parameters
+    z[batch_ix, :] = resample_latent_space(batch_x, active_x, active_z, vb_params)
